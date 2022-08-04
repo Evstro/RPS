fe9107468fd04ad12592685f99b215128b4290ea
Evstro <Evstro.code@gmail.com>
2022-08-04 16:46:36 -0500

    RPS UI
    Made the game playable through a User Interface.
    Some significant changes of the places of the functions and
    where everything is.
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -1,116 +1,115 @@
+let playerScore = 0;
+let computerScore = 0;
 
 
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
 
+rock.addEventListener('click', e => {
+    e.preventDefault();
+    const playerSelection = 'rock';
 
+    const choices = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choices.length);
 
+    let computerSelection = choices[random];
+    console.log(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    updateScore(playerScore, computerScore);
+    nextRound(playerScore, computerScore);
+});
 
+paper.addEventListener('click', e => {
+    e.preventDefault();
+    const playerSelection = 'paper';
 
+    const choices = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choices.length);
 
+    let computerSelection = choices[random];
+    console.log(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    updateScore(playerScore, computerScore);
+    nextRound(playerScore, computerScore);
+});
 
-const playGame = () => {
+scissors.addEventListener('click', e => {
+    e.preventDefault();
+    const playerSelection = 'scissors';
 
-    // initial scores for player and computer
-    let playerScore = 0;
-    let computerScore = 0;
+    const choices = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choices.length);
 
-    // function to get computer and player to choose
-    function choose() {
+    let computerSelection = choices[random];
+    console.log(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    updateScore(playerScore, computerScore);
+    nextRound(playerScore, computerScore);
+});
 
-          // function to generate a random choice for computer
-        const getComputerChoice = () => {
+function computerSelects () {
+    const choices = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choices.length);
 
+    let computerSelection = choices[random];
 
-            // return the computers selection
-            return computerSelection;
-        };
+    return computerSelection;
+}
 
-        // run function to get computer choice
-        getComputerChoice();
-   
+function playRound(playerSelection, computerSelection) {
 
-    
-    };
+    console.log(`You chose ${playerSelection}, computer chose ${computerSelection}!`);
+    switch (true) {
 
-    // function to battle
-    function battle(playerSelection, computerSelection) {
-        // display results
-        console.log(`You chose ${playerSelection}, computer chose ${computerSelection}!`);
-        switch (true) {
-    
-            
-            case playerSelection.toLowerCase() === computerSelection:
-                console.log('You both chose the same! Draw!');
-    
-                break
-    
-            case playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors':
-            case playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper':
-            case playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock':
-                console.log('You win!')
-                
-                return playerScore++;
-    
-            case computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissors':
-            case computerSelection === 'scissors' && playerSelection.toLowerCase() === 'paper':
-            case computerSelection === 'paper' && playerSelection.toLowerCase() === 'rock':
-                console.log('You lose!')
-                
-                return computerScore++;
-    
-            default:
-                console.log('Invalid choice! You forfeit!');
         
-                return computerScore++;
-        };
+        case playerSelection.toLowerCase() === computerSelection:
+            console.log('You both chose the same! Draw!');
+
+            break
+
+        case playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors':
+        case playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper':
+        case playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock':
+            console.log('You win!')
+            
+            return playerScore++;
+
+        case computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissors':
+        case computerSelection === 'scissors' && playerSelection.toLowerCase() === 'paper':
+        case computerSelection === 'paper' && playerSelection.toLowerCase() === 'rock':
+            console.log('You lose!')
+            
+            return computerScore++;
+
+        default:
+            console.log('Invalid choice! You forfeit!');
+    
+            return computerScore++;
     };
-
-    // function to play round
-    function playRound() {
-
-        // function to get player choice
-        let playerSelection = prompt('Choose Rock, Paper, or Scissors');
-        console.log(playerSelection);
-
-        const choices = ['rock', 'paper', 'scissors'];
-        const random = Math.floor(Math.random() * choices.length);
-   
-        // set a variable to choose randomly
-        let computerSelection = choices[random];
-        console.log(computerSelection);
-
-        battle(playerSelection, computerSelection);
-
-        console.log(`Player Score: ${playerScore}; Computer Score: ${computerScore}`)
-    };
-
-
-    for(let round = 1; round < 6; round++) {
-        console.log(`Round ${round}`);
-        playRound();
-    };
-
-    function restartGame() {
-        let playAgain = prompt('Play again?')
-        if(playAgain.toLowerCase() == 'yes') {
-            newGame()
-        }
-    };
-
-    if(playerScore > computerScore) {
-        console.log('You won the game!')
-        restartGame()
-    } else if(playerScore < computerScore) {
-        console.log('You lost the game!')
-        restartGame()
-    } else if(playerScore == computerScore) {
-        console.log('You tied the game!')
-        restartGame()        
-    };
-
-    function newGame() {
-        playGame()
-    };
-
 };
 
-playGame()
+function updateScore (playerScore, computerScore) {
+    const score = document.getElementById('score');
+    score.innerHTML = `Player Score: ${playerScore}; Computer Score: ${computerScore}`
+}
+
+function nextRound(playerScore, computerScore) {
+    const final = document.getElementById('final');
+
+    if (playerScore === 5) {
+        rock.style.display = 'none';
+        paper.style.display = 'none';
+        scissors.style.display = 'none';
+
+        final.innerHTML = 'You win!'
+    } else if (computerScore === 5) {
+        rock.style.display = 'none';
+        paper.style.display = 'none';
+        scissors.style.display = 'none';
+
+        final.innerHTML = 'You Lose!'
+    } else {
+        
+    }
+}
